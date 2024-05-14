0a9bca7bb9515c843a3960ce6fc8152b63ef1fe9
Federico Ahumada <fedee_crc@hotmail.com>
2024-05-14 05:42:54 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace pryEtapa3Ahumada
+{
+    partial class frmEtapa3
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnBarco = new System.Windows.Forms.Button();
+            this.btnAvion = new System.Windows.Forms.Button();
+            this.pctImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(12, 370);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(154, 58);
+            this.btnAuto.TabIndex = 0;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnBarco
+            // 
+            this.btnBarco.Location = new System.Drawing.Point(220, 370);
+            this.btnBarco.Name = "btnBarco";
+            this.btnBarco.Size = new System.Drawing.Size(154, 58);
+            this.btnBarco.TabIndex = 1;
+            this.btnBarco.Text = "Barco";
+            this.btnBarco.UseVisualStyleBackColor = true;
+            this.btnBarco.Click += new System.EventHandler(this.btnBarco_Click);
+            // 
+            // btnAvion
+            // 
+            this.btnAvion.Location = new System.Drawing.Point(428, 370);
+            this.btnAvion.Name = "btnAvion";
+            this.btnAvion.Size = new System.Drawing.Size(154, 58);
+            this.btnAvion.TabIndex = 2;
+            this.btnAvion.Text = "Avion";
+            this.btnAvion.UseVisualStyleBackColor = true;
+            this.btnAvion.Click += new System.EventHandler(this.btnAvion_Click);
+            // 
+            // pctImagen
+            // 
+            this.pctImagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pctImagen.Location = new System.Drawing.Point(12, 24);
+            this.pctImagen.Name = "pctImagen";
+            this.pctImagen.Size = new System.Drawing.Size(570, 312);
+            this.pctImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctImagen.TabIndex = 3;
+            this.pctImagen.TabStop = false;
+            // 
+            // frmEtapa3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.pctImagen);
+            this.Controls.Add(this.btnAvion);
+            this.Controls.Add(this.btnBarco);
+            this.Controls.Add(this.btnAuto);
+            this.Name = "frmEtapa3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Etapa 3 - CREAR OBJETOS POR CÓDIGO";
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnBarco;
+        private System.Windows.Forms.Button btnAvion;
+        private System.Windows.Forms.PictureBox pctImagen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEtapa3Ahumada
+{
+    public partial class frmEtapa3 : Form
+    {
+        //Creo instancias de la clase clsVehiculo (para acceder a las propiedades y metodos de la clase clsVehiculo)
+        private clsVehiculo auto;
+        private clsVehiculo barco;
+        private clsVehiculo avion;
+
+        public frmEtapa3()
+        {
+            InitializeComponent();
+
+            auto = new clsVehiculo("Auto", "Auto", Properties.Resources.auto);
+            barco = new clsVehiculo("Barco", "Barco", Properties.Resources.barco);
+            avion = new clsVehiculo("Avion", "Avion", Properties.Resources.avion);
+        }
+
+        private void MostrarVehiculo(clsVehiculo vehiculo)
+        {
+            pctImagen.Image = vehiculo.Imagen;
+            pctImagen.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            MostrarVehiculo(auto);
+        }
+
+        private void btnBarco_Click(object sender, EventArgs e)
+        {
+            MostrarVehiculo(barco);
+        }
+
+        private void btnAvion_Click(object sender, EventArgs e)
+        {
+            MostrarVehiculo(avion);
+        }
+    }
+}
